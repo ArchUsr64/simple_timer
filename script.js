983fa6ebac643863aaa0c8bd98dffe6485a4a74f
ArchUsr64 <anshulusr@gmail.com>
2023-02-04 17:15:42 +0530

    Implemented basic timer

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const main_view = document.getElementById("main_view");
-console.log("Hello World");
-setTimeout(() => {
-	main_view.style.height = "50vh";
-}, 2000);
+const main_view = document.getElementById('main_view');
+const SECONDS_IN_MINUTE = 60;
+let duration =
+    parseFloat(prompt('Enter the timer duration in minutes:', '5'));
+const TIMER_DURATION = duration * SECONDS_IN_MINUTE;
+let height = 100;
+console.log(TIMER_DURATION);
+setInterval(() => {
+  if (height > 0) {
+    height -= 0.1;
+    main_view.style.height = height + 'vh';
+  } else {
+    main_view.style.backgroundColor = 'red';
+    main_view.style.height = '100vh';
+  }
+}, TIMER_DURATION);
