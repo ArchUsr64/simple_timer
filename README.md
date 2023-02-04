3400d68f3f5abca26258a8eeb50a303897550b2f
ArchUsr64 <anshulusr@gmail.com>
2023-02-04 18:00:52 +0530

    Implemented alarm alert

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Simple Timer Web Application
+Sound Effect from [Pixabay](https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=6151)
--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const main_view = document.getElementById('main_view');
+const timer_sound = document.getElementById('timer_sound');
 const SECONDS_IN_MINUTE = 60;
-let duration =
-    parseFloat(prompt('Enter the timer duration in minutes:', '5'));
+let duration = parseFloat(prompt('Enter the timer duration in minutes:', '5'));
 const TIMER_DURATION = duration * SECONDS_IN_MINUTE;
 let height = 100;
-console.log(TIMER_DURATION);
+let alerted = false;
 setInterval(() => {
   if (height > 0) {
     height -= 0.1;
     main_view.style.height = height + 'vh';
-  } else {
+  } else if (!alerted) {
     main_view.style.backgroundColor = 'red';
     main_view.style.height = '100vh';
+    timer_sound.play();
+    setTimeout(() => {
+      timer_sound.pause();
+      timer_sound.currentTime = 0;
+      alerted = true;
+    }, 5000);
   }
 }, TIMER_DURATION);
