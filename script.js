664cb9d6f70574070086ecc5a456183b5e26ce79
ArchUsr64 <anshulusr@gmail.com>
2023-02-04 18:52:28 +0530

    Added clange formatter

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ const TIMER_DURATION = duration * SECONDS_IN_MINUTE;
 let height = 100;
 let alerted = false;
 main_view.addEventListener('click', function() {
-  main_view.style.backgroundColor = 'blue';
-  setInterval(() => {
-    if (height > 0) {
-      height -= 0.1;
-      main_view.style.height = height + 'vh';
-    } else if (!alerted) {
-      main_view.style.backgroundColor = 'red';
-      main_view.style.height = '100vh';
-      timer_sound.play();
-      setTimeout(() => {
-	timer_sound.pause();
-	timer_sound.currentTime = 0;
-	alerted = true;
-      }, 5000);
-    }
-  }, TIMER_DURATION);
+	main_view.style.backgroundColor = 'blue';
+	setInterval(() => {
+		if (height > 0) {
+			height -= 0.1;
+			main_view.style.height = height + 'vh';
+		} else if (!alerted) {
+			main_view.style.backgroundColor = 'red';
+			main_view.style.height = '100vh';
+			timer_sound.play();
+			setTimeout(() => {
+				timer_sound.pause();
+				timer_sound.currentTime = 0;
+				alerted = true;
+			}, 5000);
+		}
+	}, TIMER_DURATION);
 });
 main_view.style.backgroundColor = 'green';
